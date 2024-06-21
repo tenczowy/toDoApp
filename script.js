97a435e2581d34e8cb36e98e2c4861d95b3091b1
Daniel <danielbrodowski97@gmail.com>
2024-06-21 09:44:31 +0100

    added functionality of adding removing and clearing items from list

---
+++ script.js
@@ -0,0 +1,80 @@
+let allTasks = [
+  { id: 1, task: 'make food', completed: false },
+  { id: 2, task: 'go to work', completed: false },
+  { id: 3, task: 'have fun', completed: 1 },
+];
+
+const listOfTasks = document.querySelector('.to-do-list');
+
+const clearListBtn = document.querySelector('.clear-list');
+const addNewTaskBtn = document.querySelector('.add-new-button');
+const closeBtn = document.querySelector('.close');
+
+function displayTasks() {
+  listOfTasks.innerHTML = '';
+  allTasks.forEach((task) => {
+    const htmlTemplate = `
+    <li ${task.completed ? 'class=completed' : null} id='${task.id}'>
+              ${task.task}
+              <div class="icons">
+                <ion-icon
+                  name="${task.completed ? 'ellipse' : 'ellipse-outline'}"
+                  class="icon-not-complited"
+                ></ion-icon>
+                <ion-icon name="close"></ion-icon>
+              </div>
+            </li>
+    `;
+
+    listOfTasks.insertAdjacentHTML('beforeend', htmlTemplate);
+  });
+}
+
+function toggleCompleted(id) {
+  const task = allTasks.find((el) => el.id == id);
+  if (task) {
+    task.completed = !task.completed;
+  }
+  displayTasks();
+}
+
+function removeTask(taskId) {
+  allTasks = allTasks.filter((task) => task.id != taskId);
+  displayTasks();
+}
+
+function addTask() {
+  const enteredTask = document.querySelector('.add-new-textarea');
+  if (!enteredTask.value) return;
+
+  allTasks.push({
+    id: generateRandomID(),
+    task: enteredTask.value,
+    completed: false,
+  });
+  enteredTask.value = '';
+  displayTasks();
+}
+
+function clearList() {
+  listOfTasks.innerHTML = '';
+  allTasks.length = 0;
+  console.log(allTasks);
+}
+
+function generateRandomID() {
+  return Math.random().toString(36).substr(2, 9);
+}
+
+displayTasks();
+clearListBtn.addEventListener('click', (e) => clearList());
+addNewTaskBtn.addEventListener('click', (e) => addTask());
+listOfTasks.addEventListener('click', (e) => {
+  const id = e.target.closest('li').id;
+
+  if (e.target.name === 'close') {
+    removeTask(id);
+  } else {
+    toggleCompleted(id);
+  }
+});
